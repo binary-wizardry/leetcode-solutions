72f21b438a77af567afaef207766a68d63910131
Anton <efimov.10.111@gmail.com>
2025-09-07 19:34:40 +0300

    Create Find N Unique Integers Sum up to Zero.py

---
+++ Easy/Find N Unique Integers Sum up to Zero.py
@@ -0,0 +1,9 @@
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        result = []
+        for num in range(1, n // 2 + 1):
+            result.append(-num)
+            result.append(num)
+        if n % 2:
+            result.append(0)
+        return result
