918b9132f66b0911a46c86696576f28fcafc06e8
Anton <efimov.10.111@gmail.com>
2026-03-29 10:23:51 +0300

    Create Check if Strings Can be Made Equal With Operations I.py

---
+++ Easy/Check if Strings Can be Made Equal With Operations I.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        odd_s1 = Counter(s1[i] for i in range(1, len(s1), 2))
+        even_s1 = Counter(s1[i] for i in range(0, len(s1), 2))
+
+        odd_s2 = Counter(s2[i] for i in range(1, len(s1), 2))
+        even_s2 = Counter(s2[i] for i in range(0, len(s1), 2))
+
+        return odd_s1 == odd_s2 and even_s1 == even_s2
