bea3c49446d25f00bd4d41843153439bfe7b79a3
Anton <efimov.10.111@gmail.com>
2026-05-20 08:35:50 +0300

    Update Find the Prefix Common Array of Two Arrays.py

--- Medium/Find the Prefix Common Array of Two Arrays.py
+++ Medium/Find the Prefix Common Array of Two Arrays.py
@@ -1,10 +1,15 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        result = []
-        hash_A = set()
-        hash_B = set()
-        for i in range(len(A)):
-            hash_A.add(A[i])
-            hash_B.add(B[i])
-            result.append(len(hash_A & hash_B))
-        return result
+        set1, set2 = set(), set()
+        n = len(A)
+        prefix = [0] * n
+        
+        for i in range(n):
+            a, b = A[i], B[i]
+            set1.add(a)
+            prefix[i] += b in set1
+            prefix[i] += a in set2
+            set2.add(b)
+        
+        prefix = list(accumulate(prefix))
+        return prefix
