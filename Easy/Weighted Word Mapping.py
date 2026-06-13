11fa83972123ecd99b23f513aeb33fa09d68ff63
Anton <efimov.10.111@gmail.com>
2026-06-13 12:51:52 +0300

    Create Weighted Word Mapping.py

---
+++ Easy/Weighted Word Mapping.py
@@ -0,0 +1,5 @@
+class Solution:
+    def mapWordWeights(self, words: List[str], weights: List[int]) -> str:
+        return ''.join(chr(ord('z') - w) for w in (sum(weights[ord(c) - ord('a')] for c in word) % 26 for word in words))
+
+# one line just for fun
